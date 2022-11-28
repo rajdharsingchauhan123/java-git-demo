dc1acac41613f8d1df4a38d4141720f432bf7476
raj <raj@gmail.com>
2022-11-28 16:39:38 +0530

    add new code

---
+++ src/main/java/Macheine.java
@@ -0,0 +1,10 @@
+public class Macheine {
+    public void start(){
+        System.out.println("macheine is sarted");
+    }
+    public void stop(){
+        System.out.println("macheine is stop");
+    }
+
+
+}
