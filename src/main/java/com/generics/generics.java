88a8af758ac434828e7f633b8b14c58b8e0b2c62
raj <raj@gmail.com>
2022-11-28 15:29:40 +0530

    java generics

---
+++ src/main/java/com/generics/generics.java
@@ -0,0 +1,23 @@
+package com.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class generics {
+    public static void main(String[] args) {
+        System.out.println((method(2,5)));
+        System.out.println((method(2,"raj")));
+        System.out.println((method("taj",'r')));
+
+
+
+    }
+    static <T>List<T> method(T s1 ,T s2)
+    {
+    List<T> list =new ArrayList<>();
+    list.add(s1);
+    list.add(s2);
+    return list;
+    }
+
+}
